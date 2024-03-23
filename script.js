6512493728860483301d54c4f1c98d346804bbaa
suhaani-g <sg3836@drexel.edu>
2024-03-23 12:25:33 -0400

    Typing Animation

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+const text = "";
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 100); // Adjust typing speed
+    }
+}
+
+type();
